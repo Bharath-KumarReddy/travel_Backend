ad151b40e3e20ca035ab03c3ef80c07dfcea9c4d
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2025-07-13 11:57:20 +0530

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect('mongodb+srv://bharathkumar:bharathkumar@cluster0.xhxxcec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     }catch(err){
         console.log(err)
     }
